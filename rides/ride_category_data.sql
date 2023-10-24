0026ba4cf68c06cd0dfc9c9905cb078b5448d44c
ChingLingTung <littlesmall0540@gmail.com>
2023-10-24 10:36:03 +0800

    資料庫相關資料

---
+++ rides/ride_category_data.sql
@@ -0,0 +1,4 @@
+INSERT INTO rides.ride_category (ride_category_name,ride_category_description,height_requirement) VALUES
+	 ('兒童友善','規劃給兒童的設施',0),
+	 ('親子同樂','規劃給家庭大小同遊',100),
+	 ('刺激冒險','規劃給喜歡刺激的冒險者',140);
---
+++ rides/theme_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO rides.theme (theme_name) VALUES
+	 ('水世界'),
+	 ('冒險之旅'),
+	 ('慢樂悠遊'),
+	 ('樂高天堂');
