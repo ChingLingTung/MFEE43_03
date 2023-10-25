004220617c09e25dfc6b28873722828b53a4e2ab
ChingLingTung <littlesmall0540@gmail.com>
2023-10-25 08:42:12 +0800

    Merge ride into test1

--- rides/DB-php/DB-CRUD/parts/scripts.php
+++
@@ -1,5 +0,0 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/1000hz-bootstrap-validator/0.11.9/validator.min.js"></script>
-
-
--- rides/theme_data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO rides.theme (theme_name) VALUES
-	 ('水世界'),
-	 ('冒險之旅'),
-	 ('慢樂悠遊'),
-	 ('樂高天堂');
